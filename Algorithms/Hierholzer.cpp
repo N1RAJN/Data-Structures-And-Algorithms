c7ea1f2f7816ce7b36ee766b326622e6eac788ff
Nirajan <nirajanacharya787@gmail.com>
2026-07-01 13:27:27 +0545

    Heirholzer alogirthm for finding Eulear path in a directed graph

---
+++ Algorithms/Hierholzer.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findStartNode(int n, const vector<int> &inDegrees,
+                  const vector<int> &outDegrees) {
+    int start = 0;
+    for (int v = 0; v < n; ++v) {
+        if (outDegrees[v] - inDegrees[v] == 1)
+            return v;
+        if (outDegrees[v] > 0)
+            start = v;
+    }
+    return start;
+}
+
+bool hasEulerPath(int n, const vector<int> &inDegrees,
+                  const vector<int> &outDegrees, int edgeCount) {
+    if (edgeCount == 0)
+        return false;
+
+    int startNodes = 0, endNodes = 0;
+    for (int v = 0; v < n; ++v) {
+        int diff = outDegrees[v] - inDegrees[v];
+        int absDiff = diff < 0 ? -diff : diff;
+        if (absDiff > 1)
+            return false;
+        else if (diff == 1)
+            startNodes++;
+        else if (diff == -1)
+            endNodes++;
+    }
+    return (startNodes == 0 && endNodes == 0) ||
+           (startNodes == 1 && endNodes == 1);
+}
+
+void dfs(vector<vector<int>> &adjList, vector<int> &outDegree,
+         vector<int> &path, int n, int v) {
+    while (outDegree[v] != 0) {
+        outDegree[v]--;
+        // Use the size of outdegree array to choose the next edge
+        int u = adjList[v][outDegree[v]];
+        dfs(adjList, outDegree, path, n, u);
+    }
+    path.push_back(v);
+}
+
+vector<int> heirholzer(vector<vector<int>> &adjList) {
+    int n = adjList.size();
+    int edgeCount = 0;
+
+    vector<int> inDegree(n, 0);
+    vector<int> outDegree(n, 0);
+    vector<int> path;
+
+    for (int v = 0; v < n; ++v) {
+        for (int u : adjList[v]) {
+            inDegree[u]++;
+            outDegree[v]++;
+            edgeCount++;
+        }
+    }
+
+    if (!hasEulerPath(n, inDegree, outDegree, edgeCount))
+        return path;
+
+    int start = findStartNode(n, inDegree, outDegree);
+    dfs(adjList, outDegree, path, n, start);
+
+    // Disconnected graph
+    if (path.size() != edgeCount + 1)
+        return {};
+    reverse(path.begin(), path.end());
+    return path;
+}
